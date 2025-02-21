0b8bd9f46f701390d19a68dc23a96598b2fc84a7
Nick Glavin <nickg@benon.com>
2025-02-22 06:31:34 +1000

    fix: Correct days calculation in getOffsetFromDate function to exclude the end date

--- examples/app/src/utils/getOffsetFromDate.ts
+++ examples/app/src/utils/getOffsetFromDate.ts
@@ -3,6 +3,6 @@ import { differenceInDays } from "date-fns";
 import { getWidthFromDays } from "./getWidthFromDays";
 
 export const getOffsetFromDate = (taskStart: Date, ganttStart: Date, gridWidth: number): number => {
-	const daysToDate = differenceInDays(taskStart, ganttStart) + 1;
+	const daysToDate = differenceInDays(taskStart, ganttStart);
 	return getWidthFromDays(daysToDate, gridWidth);
 };
